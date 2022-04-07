991cb57136e702493cf462d0c3f8b65cc435c61b
fadhilsaheer <fadhilsaheer@gmail.com>
2022-04-07 12:34:05 +0530

    http request

---
+++ src/http/http_request.rs
@@ -0,0 +1,35 @@
+use super::types;
+use reqwest::{Client, Method, Response};
+
+pub async fn http_request(
+    client: &Client,
+    request: types::Request,
+) -> Result<types::Response, types::Error> {
+    let resp = client
+        .request(request.method.clone(), &request.url)
+        .json(&request.body)
+        .headers(request.headers)
+        .send()
+        .await;
+
+    handle_response(resp, request.url, request.method).await
+}
+
+async fn handle_response(
+    resp: Result<Response, reqwest::Error>,
+    url: String,
+    method: Method,
+) -> Result<types::Response, types::Error> {
+    match resp {
+        Err(err) => Err(types::Error::FailedToFetch(err)),
+        Ok(response) => {
+            let status = response.status();
+            let result = response.text().await;
+
+            match result {
+                Ok(data) => Ok(types::Response::new(status, data)),
+                Err(_) => Err(types::Error::FailedToParseResponse(method, url)),
+            }
+        }
+    }
+}
